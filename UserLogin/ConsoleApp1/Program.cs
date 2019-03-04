fa1c3de24646211d7953f883dfc2c1947ca29ea3
MuyLindo <i.tabutova@gmail.com>
2019-03-04 18:01:54 +0200

    TU Practice 01

---
+++ UserLogin/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Username = "Somebody";
+            user.Password = "12346";
+            user.FacultyNumb = "121216306";
+            user.Role = 1;
+
+            Console.WriteLine(user.Username);
+            Console.WriteLine(user.Password);
+            Console.WriteLine(user.FacultyNumb);
+
+            switch (user.Role)
+            {
+
+                case 1:
+                    Console.WriteLine("Administrator"); break;
+                case 2:
+                    Console.WriteLine("Student"); break;
+
+                case 3:
+                    Console.WriteLine("Student"); break;
+
+                case 4:
+                    Console.WriteLine("Student"); break;
+
+                default: Console.WriteLine("Incorrect number!"); break;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ UserLogin/ConsoleApp1/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        public System.String Username;
+        public System.String Password;
+        public System.String FacultyNumb;
+        public System.Int32 Role;
+
+      
+
+    }
+}

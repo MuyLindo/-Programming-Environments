611788cfed652dc4b1e7e5f5b7a392e0e28e8a29
MuyLindo <i.tabutova@gmail.com>
2019-03-17 14:10:57 +0200

    Ignore

--- SecondPart/SecondPart/UserData.cs
+++ SecondPart/SecondPart/UserData.cs
@@ -9,29 +9,38 @@ namespace SecondPart
     static class UserData
     {
        // public static User TestUser = new User();
-        private static User _testUser = new User();
+        private static User[] _testUser = new User[5];
 
         public static User TestUser
         {
             get
             {
                 ResetTestUserData();
-                return _testUser;
+                for(int i = 0; i < _testUser.Length; i++)
+                {
+                    return _testUser[i];
+                }
+                
             }
             set { }
         }
 
         private static void ResetTestUserData()
         {
+            User _testUser[0] = UserData("Name", "pass");
+            
+            /*
             _testUser.Username = "Gosho";
             _testUser.Password = "123456";
             _testUser.FacultyNumber = "121216001";
             _testUser.Role = 1;
+            */
         }
 
         public static User IsUserPassCorrect(String Username, String Password)
         {
 
+            return ;
         }
     }
 }

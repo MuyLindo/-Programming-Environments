d2331695ac825839f6ef0d119b4805d6c6498702
MuyLindo <i.tabutova@gmail.com>
2019-03-17 19:43:29 +0200

    Student

--- SecondPart/StudentRepository/Student.cs
+++ SecondPart/StudentRepository/Student.cs
@@ -8,5 +8,17 @@ namespace StudentRepository
 {
     class Student
     {
+        public String Name { get; set; }
+        public String SecondName { get; set; }
+        public String ThirdName { get; set; }
+        public String Faculty { get; set; }
+        public String Speciality { get; set; }
+        public String Degree { get; set; }
+        public String Status { get; set; }
+        public String FacultyNumber { get; set; }
+        public Int32 Course { get; set; }
+        public Int32 Stream { get; set; }
+        public Int32 Group { get; set; }
+
     }
 }
